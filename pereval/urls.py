16d65ac419c111f3a1c76b42d2436053d145e76e
nikulson <nikulshin@mail.ru>
2022-12-14 12:18:52 +0400

    swagger

--- pereval/urls.py
+++ pereval/urls.py
@@ -13,20 +13,28 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 from rest_framework import routers
-from mounts.views import UserViewSet, AreaViewSet, MountainPassViewSet, submitData
+from mounts.views import UserViewSet, AreaViewSet, MountainPassViewSet, submitData, mountain_pass_get_patch
+from .yasg import urlpatterns as doc_urls
 
 router = routers.DefaultRouter()
 router.register('MountainPass', MountainPassViewSet)
 router.register(r'user', UserViewSet)
 router.register(r'area', AreaViewSet)
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include(router.urls)),
-    path('api/v1/submitData/', submitData)
+    path('api/v1/submitData/', submitData),
+    path('api/v1/<int:mountain_pass_id>', mountain_pass_get_patch)
 ]
+
+urlpatterns += doc_urls
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
