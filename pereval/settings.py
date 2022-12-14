ee13b39dacc0184f56caa689d65f5305561b4567
nikulson <nikulshin@mail.ru>
2022-12-14 22:19:45 +0400

    add_dotenv

--- pereval/settings.py
+++ pereval/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 
+
+SECRET_KEY = os.environ['SECRET_KEY']
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -19,8 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ri7-!u9)jakqp#yi16!4_fi5@t8wavfnx8m%sxzso9g$%y31b3'
 
+SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
